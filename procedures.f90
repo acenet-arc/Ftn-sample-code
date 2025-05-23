9249e35f934c0226d65dd5b4eb18e6f4c6f566dc
Ross Dickson <ross.dickson@ace-net.ca>
2025-05-23 20:58:50 +0000

    add module & use to procedures.f90

--- procedures.f90
+++ procedures.f90
@@ -1,10 +1,14 @@
 program main
+   use procedures_mod
    real :: a = 10.5, b = 2.0
    print *, a, b
    call swapreals(a, b)
    print *, a, b
    print *, proportion(a, b)
 end program main
+
+module procedures_mod
+contains
 
 subroutine swapreals(x, y)
    real :: x, y, t
@@ -15,3 +19,5 @@ function proportion(x, y)
    real :: x, y, proportion  ! matches name of function
    proportion = (x-y)/(x+y)
 end function proportion
+
+end module procedures_mod
