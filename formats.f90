8317de434d49ce787480509c47b6e505ef736ffb
Ross Dickson <ross.dickson@ace-net.ca>
2025-06-05 19:32:28 +0000

    expand formats.f90

--- formats.f90
+++ formats.f90
@@ -4,11 +4,14 @@ real :: a, b
 integer :: i, j
 print *, 'Enter two floating point numbers and two integers, please:'
 read *, a, b, i, j
+print *
+print *, 'Default (list-directed) formatting:'
 print *, a, b
+print *
+print *, 'Formatted 2F10.1:'
 print 100, a, b
 100 format(2F10.1)
 print 200
-200 format('12345678901234567890')
-print 300, i, j
-300 format(2I10)
+200 format(/,'12345678901234567890')  ! / is newline, aka "vertical tab"
+print '(2I10)', i, j                  ! format can be inline instead of a statement
 end program
