0ac127ab012301603a53b873a49f1e89c9c4fc68
Ross Dickson <ross.dickson@ace-net.ca>
2026-06-03 17:37:48 -0300

    fix module def in procedures.f90

--- procedures.f90
+++ procedures.f90
@@ -1,3 +1,20 @@
+module procedures_mod
+contains
+
+subroutine swapreals(x, y)
+   real,intent(inout) :: x, y
+   real :: t
+   t = x; x = y; y = t
+end subroutine swapreals
+
+function proportion(x, y)
+   real,intent(in) :: x, y
+   real :: proportion  ! matches name of function
+   proportion = (x-y)/(x+y)
+end function proportion
+
+end module procedures_mod
+
 program main
    use procedures_mod
    real :: a = 10.5, b = 2.0
@@ -6,18 +23,3 @@ program main
    print *, a, b
    print *, proportion(a, b)
 end program main
-
-module procedures_mod
-contains
-
-subroutine swapreals(x, y)
-   real :: x, y, t
-   t = x; x = y; y = t
-end subroutine swapreals
-
-function proportion(x, y)
-   real :: x, y, proportion  ! matches name of function
-   proportion = (x-y)/(x+y)
-end function proportion
-
-end module procedures_mod
